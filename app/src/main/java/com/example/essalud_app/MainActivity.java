d30b65fe2f4c04fd080c098401c95a759a7f32c6
Limonada16-ucsm <75735731+Limonada16-ucsm@users.noreply.github.com>
2020-12-09 08:44:11 -0500

    Cambios loquitos 2

--- app/src/main/java/com/example/essalud_app/MainActivity.java
+++ app/src/main/java/com/example/essalud_app/MainActivity.java
@@ -10,10 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        asdasdasdasdsadas
-    }
-    protected void onSave(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
 }
