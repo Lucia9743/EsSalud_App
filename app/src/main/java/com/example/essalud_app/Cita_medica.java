87e0aa750897218f3fee0d8c284e4ee5557d99d8
Lucia9743 <66081365+Lucia9743@users.noreply.github.com>
2020-12-09 15:13:31 -0500

    Pantalla de inicio-registro

---
+++ app/src/main/java/com/example/essalud_app/Cita_medica.java
@@ -0,0 +1,14 @@
+package com.example.essalud_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cita_medica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cita_medica);
+    }
+}
---
+++ app/src/main/java/com/example/essalud_app/Resumen_cita.java
@@ -0,0 +1,14 @@
+package com.example.essalud_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Resumen_cita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumen_cita);
+    }
+}
