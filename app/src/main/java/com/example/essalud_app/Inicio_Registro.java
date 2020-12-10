75e6a6c41451e13163d8ab535a49d5c5cb8eb8d4
Limonada16-ucsm <75735731+Limonada16-ucsm@users.noreply.github.com>
2020-12-10 14:03:07 -0500

    SplashScreen, Funcionalidad botones

--- app/src/main/java/com/example/essalud_app/Inicio_Registro.java
+++ app/src/main/java/com/example/essalud_app/Inicio_Registro.java
@@ -2,7 +2,10 @@ package com.example.essalud_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio_Registro extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class Inicio_Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio__registro);
+        Button btnRegistrarse = (Button) findViewById(R.id.btnRegis);
+        Button btnIniSesion = (Button)findViewById(R.id.btnIniSesi);
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Inicio_Registro.this, Registro_Usuario.class);
+                startActivity(intent1);
+            }
+        });
+
+        btnIniSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(Inicio_Registro.this, Inicio_Sesion.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
