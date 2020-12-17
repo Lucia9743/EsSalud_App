d99a4c04466e70ba7094c55708be478bad41fc40
Lucia9743 <66081365+Lucia9743@users.noreply.github.com>
2020-12-17 14:46:49 -0500

    pantalla neurologia y peditaria

--- app/src/main/java/com/example/essalud_app/Medicina_general.java
+++ app/src/main/java/com/example/essalud_app/Medicina_general.java
@@ -1,15 +1,20 @@
 package com.example.essalud_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Medicina_general extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button button;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicina_general);
         getSupportActionBar().hide();
+        button = (Button) findViewById(R.id.button);
     }
 }
